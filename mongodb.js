872d57227ee01d655d8f6ed7796205ee2c980aa7
daegeunkim <daegeunkim92@gmail.com>
2020-05-31 23:05:16 +0900

    Add remark

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 const member = require('./schema')
 
+/*
+This it for mongoose connection test & practice for CRUD
+*/
+
 mongoose.Promise = global.Promise
 mongoose.set('useUnifiedTopology', true) //prevent deprecation warnings
 mongoose.connection.on('error', (err) => { if(err) console.log(err)} )
@@ -12,56 +16,58 @@ mongoose.connect('mongodb://localhost:27017/testdb', {useNewUrlParser: true}, (e
 
 //create
 var newMember = {id: 1, name: 'paul'}
-// member.create(newMember, (err) => {
-//     if(err){
-//         console.log(err)
-//         return
-//     }
-//     console.log('Created!!')
+member.create(newMember, (err) => {
+    if(err){
+        console.log(err)
+        return
+    }
+    console.log('Created!!')
 
-//     //Read after create
-//     member.find((err, res) => {
-//         if(err) {
-//             console.log(err)
-//             return
-//         }
-//         console.log('Find One')
-//         console.log(res)
-//         mongoose.disconnect()
-//     })
-// })
+    //Read after create
+    member.find((err, res) => {
+        if(err) {
+            console.log(err)
+            return
+        }
+        console.log('Find All')
+        console.log(res)
+        mongoose.disconnect()
+    })
+})
 
 
-// member.deleteOne(newMember, (err) => {
-//     if(err) {
-//         console.log(err)
-//         return
-//     }
+member.deleteOne(newMember, (err) => {
+    if(err) {
+        console.log(err)
+        return
+    }
+    console.log('Delete One')
 
-//     member.find(function(err, res){
-//         if(err) {
-//             console.log(err)
-//             return
-//         }
-//         console.log('Find All')
-//         console.log(res)
-//         mongoose.disconnect()
-//     })
-// })
+    member.find((err, res) => {
+        if(err) {
+            console.log(err)
+            return
+        }
+        console.log('Find All')
+        console.log(res)
+        mongoose.disconnect()
+    })
+})
 
-// var updateMember = {id: 1, name: 'david'}
-// member.updateOne(newMember, updateMember, (err, raw) => {
-//     if(err) {
-//         console.log(err)
-//         return
-//     }
-//     member.find((err, res) => {
-//     if(err) {
-//         console.log(err)
-//         return
-//     }
-//     console.log('Find One')
-//     console.log(res)
-//     mongoose.disconnect()
-//     })
-// })
+var updateMember = {id: 1, name: 'david'}
+member.updateOne(newMember, updateMember, (err, raw) => {
+    if(err) {
+        console.log(err)
+        return
+    }
+    console.log('Update One')
+    member.find((err, res) => {
+        if(err) {
+            console.log(err)
+            return
+        }
+        console.log('Find One')
+        console.log(res)
+        mongoose.disconnect()
+    })
+})
